497b018745a5633358769834271c08152d30fa29
Soban <sobanfarooq911@gmail.com>
2025-02-02 18:55:29 +0530

    added monitoring logic in flask app

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import pickle, os, argparse
+import pickle, os, json
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -13,12 +13,10 @@ class RandomForrest:
 
     def load(self):
         latest_model_path = None
-        model_folder_path = './model'
         max_date = None
 
-        for folder in os.listdir(model_folder_path):
-            if os.path.isdir(os.path.join(model_folder_path, folder)):
-                print(folder)
+        for folder in os.listdir('./model'):
+            if os.path.isdir(os.path.join('./model', folder)):
                 datestr = folder.split('v')[1]
                 date = datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
                 if (max_date is None) or (date > max_date):
@@ -27,13 +25,30 @@ class RandomForrest:
         
         if latest_model_path is None:
             raise("No model folder was found, please train a mode first!")
+        
+        self.model_dir_path = os.path.join('./model', latest_model_path)
+        pickle_file_path = os.path.join('./model', latest_model_path, 'model.pkl')
 
-        model_path = os.path.join(model_folder_path, latest_model_path, 'model.pkl')
+        with open(pickle_file_path, 'rb') as pickle_file:
+            self.model = pickle.load(pickle_file)
 
-        with open(model_path, 'rb') as model_file:
-            self.model = pickle.load(model_file)
+        print("Latest Model Loaded : ", pickle_file_path)
 
-        print("Latest Model Loaded : ", model_path)
+
+    def log_data(self, data):
+        log_file_path = os.path.join(self.model_dir_path, 'log.json')
+        if os.path.exists(log_file_path):
+            # If the file exists, append to it
+            with open(log_file_path, 'r+') as f:
+                logs = json.load(f)
+                logs.append(data)
+                f.seek(0)
+                json.dump(logs, f, indent=4)
+        else:
+            # If the file doesn't exist, create a new file and write the data
+            with open(log_file_path, 'w') as f:
+                json.dump([data], f, indent=4)
+
 
 randomForrest = RandomForrest()
 
@@ -47,8 +62,14 @@ def recommend():
     data = np.array([data])
     data_df = pd.DataFrame(data, columns=features)
     prediction = int(randomForrest.model.predict(data_df)[0])
-
     res = "Diabetic" if prediction else "Not Diabetic"
+
+    randomForrest.log_data({
+        "request": payload,
+        "response": res,
+        "time" : datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    })
+    
     return "Patient is " + res
 
 if __name__ == '__main__':
